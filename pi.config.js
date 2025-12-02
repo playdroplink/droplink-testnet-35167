1fc780f3431fbf7d06d890ef2bb29183bf6cb7a2
reimaginetruth <reimaginetruth@example.com>
2025-12-03 05:08:11 +0800

    feat: Update API key in Pi integration configuration for enhanced security

--- pi.config.js
+++ pi.config.js
@@ -3,5 +3,5 @@
 
 module.exports = {
   validationKey: "7511661aac4538b1832d2c9ba117f6d972b26a54640598d3fbb9824013c7079203f65b02d125be3f418605cfb89ba0e4443e3ec997e3800eb464df0bc5410d2a",
-  apiKey: "rfjwwiyaysjsinhjrkhvplkrrmf5rjnwd3blmzuxdmek1t5mhy4avy6s6k96znwc"
+  apiKey: "b00j4felp0ctc1fexe8igldsjg9u7wbqitavc15si53fr9wwra7r6oluzk4j24qz"
 };
